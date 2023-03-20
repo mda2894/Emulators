c7ba78d8f80b9badf77a53ac464657d317e23845
Michael Anderson <mda2894@gmail.com>
2023-03-19 22:42:20 -0400

    Moved notes to separate text file
    Added notes

--- SAP1/SAP1.py
+++ SAP1/SAP1.py
@@ -7,12 +7,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-# improvements:
-# - implement Register class, and (possibly?) some sort of inheritance/dependence with the memory class
-# - implement instruction decoding with dictionary instead of match/case
-# - add ability to display full CPU state at once (registers, PC, IR, MAR, memory, etc)
-# - implement clock cycling, with properly timed microinstructions
-# - bus??
